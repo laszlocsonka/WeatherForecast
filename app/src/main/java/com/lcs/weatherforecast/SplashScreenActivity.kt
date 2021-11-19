0c6e42337d7f1e73dc85d456c12400c953a37fcd
laszlocsonka <43496943+laszlocsonka@users.noreply.github.com>
2021-11-19 12:42:31 +0100

    Weather Forecast Android App

---
+++ app/src/main/java/com/lcs/weatherforecast/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.lcs.weatherforecast
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        val handler = Handler();
+
+        handler.postDelayed (  {
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000);
+
+    }
+}
